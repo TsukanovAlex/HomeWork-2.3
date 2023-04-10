9a486d9924573b9d149d18267b11fbb5de333a66
*Aleksey <*atsukanov89@gmail.com*>
2023-04-10 18:10:41 +0400

    Выполнено задание №5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// // Задание №1
+// Задание №1
 let password = 'test';
 let interPassword = prompt('Введите пароль');
 if (password === interPassword) {
@@ -6,7 +6,7 @@ if (password === interPassword) {
 } else {
     alert('Пароль введен неправильно');
 }
-// // Задание №1 с помощью тернарного оператора.
+// Задание №1 с помощью тернарного оператора.
 let password1 = 'test';
 let interPassword1 = prompt('Введите пароль');
 password1 === interPassword1 ? alert('Пароль введен верно') : alert('Пароль введен неправильно');
@@ -25,3 +25,30 @@ let a = 2;
 let b = 3;
 alert(a + b);
 
+// Задание №5
+let monthNumber = prompt('Введите номер месяца');
+switch (monthNumber) {
+    case '1':
+    case '2':
+    case '12':
+        alert('Зима');
+        break;
+    case '3':
+    case '4':
+    case '5':
+        alert('Весна');
+        break;
+    case '6':
+    case '7':
+    case '8':
+        alert('Лето');
+        break;
+    case '9':
+    case '10':
+    case '11':
+        alert('Осень');
+        break;
+    default:
+        alert('Такого месяца не сущетвует');
+        break;
+}
